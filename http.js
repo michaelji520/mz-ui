8f6ef2d0bb92b1d23ca7cf39dcbded2b1f16fc43
Michael Zhang <michaelji520@gmail.com>
2019-11-07 00:17:35 +0800

    update http.js

--- http.js
+++ http.js
@@ -33,9 +33,9 @@ var server = http.createServer((req, res) => {
         res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
         res.end();
       } else {
-        res.writeHeader(200,{
-            'content-type' : 'text/html;charset="utf-8"'
-        });
+        // res.writeHeader(200,{
+        //     'content-type' : 'text/html;charset="utf-8"'
+        // });
         res.write(data);//将index.html显示在客户端
         res.end();
       }
